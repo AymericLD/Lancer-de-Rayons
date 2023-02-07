a9cef2e447fe07c99f4220bb843299b6c9932267
OursPhilosophe <102171881+OursPhilosophe@users.noreply.github.com>
2023-02-07 12:03:52 +0100

    Update raytracer.cpp

--- raytracer.cpp
+++ raytracer.cpp
@@ -19,6 +19,36 @@ namespace constantes
 
 using namespace constantes;
 
+Grille::Grille(l,h,t)
+{
+    l_p(l);
+    h_p(h);
+    taille(t);
+    int n = l_p*h_p;
+    for(int i; i<=n;i++) { //initialisation d'une grille, k*l_p<i<(k+1)*l_p - > k_ième ligne en partant du bas, i mod k eme colonne
+        g[i].centre.x = t/2 + (i%k)*t
+        g[i].centre.y = t/2 + floor(i/k)*t;
+        g[i].centre.z = 0;
+        g[i].largeur = t;
+        g[i].rgb = (0,0,0); //initialisation du rgb à 0,0,0
+    }
+}
+void Grille::creation_image()
+{ //faire le main
+
+std::ofstream outfile ("scene.ppm"); //Mettre l'arborescence correspondant au PC de clui qui compile²
+outfile << "P3" << std::endl;
+outfile << l_p << " " << h_p << std::endl;
+outfile << "255" << std::endl;
+n = l_p*h_p;
+for(int i; i<=n; i++){
+    outfile << Grille(i)[1] << " " << Grille(i)[2] << " " << Grille(i)[3] << " " << std::endl; // Attention index
+}
+
+}
+
+// fstream pour écrire dans un fichier, format P3 -> .ppm
+
 
 // Fonction de connection rayon-source
 
